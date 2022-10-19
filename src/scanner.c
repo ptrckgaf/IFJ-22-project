61eecf42535f7762801559036e2ac2dd47100f5f
Anton Miklis <antonmiklis@icloud.com>
2022-10-19 09:01:29 +0200

    feat: function for getting next automaton state implemented

--- src/scanner.c
+++ src/scanner.c
@@ -1,1 +1,143 @@
 #include "scanner.h"
+#include "ctype.h"
+
+AutomatonState AutomatonNext(AutomatonState current, char input){
+    switch (current) {
+        case START:
+            if (input == ' ' || input == '\t' || input == '\n'){return START;}
+            if (input == '$'){return VARIABLE_DOLLAR;}
+            if (isalpha(input) || input == '_'){return IDENTIFIER;}
+            if (input == '?'){return QUESTION_MARK;}
+            if (input == '('){return LEFT_PARENTHESE;}
+            if (input == ')'){return RIGHT_PARENTHESE;}
+            if (input == '*'){return MULTIPLICATION;}
+            if (input == '+'){return PLUS;}
+            if(input == '-'){return MINUS;}
+            if (input == '>'){return GREATER;}
+            if (input == '<'){return LESS;}
+            if (input =='='){return ASSIGN;}
+            if (input =='/'){return SLASH;}
+            if (input =='"'){return STRING_START;}
+            if (isnumber(input)){return NUMBER;}
+            if (input == '!'){return EXCLAMATION_MARK;}
+
+            return ERROR;
+
+        case IDENTIFIER:
+            if (isnumber(input) || isalpha(input) || input == '_'){return IDENTIFIER;}
+            return ERROR;
+
+        case QUESTION_MARK:
+            if (isnumber(input) || isalpha(input) || input == '_'){return IDENTIFIER;}
+            return ERROR;
+
+        case STRING_START:
+            if (input == '"'){return STRING_END;}
+            if (input > 31){return IDENTIFIER;}
+            //TODO escaping sequences
+            return ERROR;
+
+        case NUMBER:
+            if (isnumber(input)){return NUMBER;}
+            if (input == '.'){return NUMBER_DOT;}
+            if (input == 'e' || input == 'E'){return NUMBER_E;}
+            return ERROR;
+
+        case NUMBER_DOT:
+            if (isnumber(input)){return NUMBER_DECIMAL;}
+            return ERROR;
+
+        case NUMBER_DECIMAL:
+            if (isnumber(input)){return NUMBER_DECIMAL;}
+            if (input == 'e' || input == 'E'){return NUMBER_E;}
+            return ERROR;
+
+        case NUMBER_E:
+            if (isnumber(input)){return NUMBER_EXPONENTIAL;}
+            if (input == '+' || input == '-'){return NUMBER_E_SIGN;}
+            return ERROR;
+
+        case NUMBER_E_SIGN:
+            if (isnumber(input)){return NUMBER_EXPONENTIAL;}
+            return ERROR;
+
+        case NUMBER_EXPONENTIAL:
+            if (isnumber(input)){return NUMBER_EXPONENTIAL;}
+            return ERROR;
+
+        case STRING:
+            if (input == '"'){return STRING_END;}
+            if (input > 31){return STRING;}
+            //TODO escape sequence
+            return ERROR;
+
+        case STRING_END:
+            return ERROR;
+
+//        case STRING_ESC:
+//            return ERROR;
+
+        case VARIABLE_DOLLAR:
+            if (isalpha(input) || input == '_'){return VARIABLE_ID;}
+            return ERROR;
+
+        case VARIABLE_ID:
+            if (isnumber(input) || isalpha(input) || input == '_'){return VARIABLE_ID;}
+            return ERROR;
+
+        case SLASH:
+            if (input == '*'){return COMMENT_BLOCK;}
+            if (input == '/'){return COMMENT;}
+            return DIVISION;
+
+        case COMMENT:
+            //TODO maybe exclude EOL
+            return COMMENT_TEXT;
+
+        case COMMENT_TEXT:
+            if (input == '\n'){return START;}
+            return COMMENT_TEXT;
+
+        case COMMENT_BLOCK:
+            if (input == '*'){return COMMENT_BLOCK_TEXT;}
+            return ERROR;
+
+        case COMMENT_BLOCK_TEXT:
+            if (input == '*'){return COMMENT_BLOCK_END;}
+            return COMMENT_BLOCK_TEXT;
+
+        case COMMENT_BLOCK_END:
+            if (input == '/'){return START;}
+            return COMMENT_BLOCK_TEXT;
+
+        case LESS:
+            if (input == '='){return LESS_EQUAL;}
+            return ERROR;
+
+        case GREATER:
+            if (input == '='){return GREATER_EQUAL;};
+            return ERROR;
+
+        case ASSIGN:
+            if (input == '='){return COMPARE1;}
+            return ERROR;
+
+        case COMPARE1:
+            if (input == '='){return COMPARE2;}
+            return ERROR;
+
+        case EXCLAMATION_MARK:
+            if (input == '='){return NEG_COMPARE1;}
+            return NEG_COMPARE1;
+
+        case NEG_COMPARE1:
+            if (input == '='){return NEG_COMPARE2;}
+            return ERROR;
+
+        default:
+            return ERROR;
+    }
+
+}
+
+
--- src/scanner.h
+++ src/scanner.h
@@ -2,29 +2,56 @@
  * automata states
  */
 #include "utils.h"
+
 typedef enum{
     START,
     IDENTIFIER,
     QUESTION_MARK,
     LEFT_PARENTHESE,
     RIGHT_PARENTHESE,
-    STRING_START,
+
+    //States for processing numbers
     NUMBER,
     NUMBER_DOT,
     NUMBER_DECIMAL,
     NUMBER_E,
     NUMBER_E_SIGN,
     NUMBER_EXPONENTIAL,
+
+    //States for processing strings
+    STRING_START,
     STRING,
     STRING_END,
     STRING_ESC,
+
     VARIABLE_DOLLAR,
     VARIABLE_ID,
+
+    //States for processing comments
     SLASH,
     COMMENT,
     COMMENT_TEXT,
     COMMENT_BLOCK,
     COMMENT_BLOCK_END,
-    COMMENT_BLOCK_TEXT
-}AutomataState;
+    COMMENT_BLOCK_TEXT,
+    ERROR,
+
+    //States to process operations
+    MULTIPLICATION,
+    DIVISION,
+    PLUS,
+    MINUS,
+    GREATER,
+    LESS,
+    GREATER_EQUAL,
+    LESS_EQUAL,
+    ASSIGN,
+    COMPARE1,
+    COMPARE2,
+    EXCLAMATION_MARK,
+    NEG_COMPARE1,
+    NEG_COMPARE2
+}AutomatonState;
+
+AutomatonState AutomatonNext(AutomatonState current, char input);
 
