c4ba931707fadc81aeb6516839ae6c83219f29af
matekli <matekli@gmail.com>
2022-12-07 22:25:31 +0100

    semantics

--- src/semantics.h
+++ src/semantics.h
@@ -1,5 +1,13 @@
 #ifndef IFJ22_SEMANTICS_H
 #define IFJ22_SEMANTICS_H
-
-int semantics();
+#include "symtable.h"
+int getVarType(int type);
+void *getParamTypes(int type,char paramTypeChar[2]);
+void semCheck(ASTstruct *tree,FSTable *ftab,char *name);
+void functionBody(ASTstruct *tree,FSTable *ftab,char *name,ASTstruct *back);
+void insert_global(ASTstruct *tree,FSTable *ftab);
+void paramsToTable(ASTstruct *tree,FSTable *ftab);
+void function_params(ASTstruct *tree,FSTable *ftab);
+void insert_function(ASTstruct *tree,FSTable *ftab);
+void semantics();
 #endif //IFJ22_SEMANTICS_H
